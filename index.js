e35c47312b6538228daeb795ce9a389624466355
Anton Medvedev <anton@elfet.ru>
2015-09-14 20:11:21 +0700

    Fallback feature.

--- index.js
+++ index.js
@@ -1,67 +1,82 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
+ MIT License http://www.opensource.org/licenses/mit-license.php
+ Author Tobias Koppers @sokra
+ */
 var ConstDependency = require("webpack/lib/dependencies/ConstDependency");
 var NullFactory = require("webpack/lib/NullFactory");
 var MissingLocalizationError = require("./MissingLocalizationError");
 
 /**
  *
- * @param {object|function}	localization
- * @param {string}			functionName
- * @param {boolean}			failOnMissing
+ * @param {object|function}    localization
+ * @param {string}            functionName
+ * @param {object|function}            fallback
  * @constructor
  */
-function I18nPlugin(localization, functionName, failOnMissing) {
-	this.localization = localization? ('function' === typeof localization? localization: makeLocalizFunction(localization))
-									: null;
+function I18nPlugin(localization, functionName, fallback) {
+	this.localization = localization ? ('function' === typeof localization ? localization : makeLocalizFunction(localization)) : null;
 	this.functionName = functionName || "__";
-	this.failOnMissing = failOnMissing || false;
+	this.fallback = fallback ? ('function' === typeof fallback ? fallback : makeLocalizFunction(fallback)) : null;
 }
 module.exports = I18nPlugin;
 
-I18nPlugin.prototype.apply = function(compiler) {
+I18nPlugin.prototype.apply = function (compiler) {
 	var localization = this.localization,
-		failOnMissing = this.failOnMissing;
-	compiler.plugin("compilation", function(compilation, params) {
+		fallback = this.fallback;
+	compiler.plugin("compilation", function (compilation, params) {
 		compilation.dependencyFactories.set(ConstDependency, new NullFactory());
 		compilation.dependencyTemplates.set(ConstDependency, new ConstDependency.Template());
 	});
-	compiler.parser.plugin("call " + this.functionName, function(expr) {
+	compiler.parser.plugin("call " + this.functionName, function (expr) {
 		var param, defaultValue;
-		switch(expr.arguments.length) {
-		case 2:
-			param = this.evaluateExpression(expr.arguments[1]);
-			if(!param.isString()) return;
-			param = param.string;
-			defaultValue = this.evaluateExpression(expr.arguments[0]);
-			if(!defaultValue.isString()) return;
-			defaultValue = defaultValue.string;
-			break;
-		case 1:
-			param = this.evaluateExpression(expr.arguments[0]);
-			if(!param.isString()) return;
-			defaultValue = param = param.string;
-			break;
-		default:
-			return;
+		switch (expr.arguments.length) {
+			case 2:
+				param = this.evaluateExpression(expr.arguments[1]);
+				if (!param.isString()) return;
+				param = param.string;
+				defaultValue = this.evaluateExpression(expr.arguments[0]);
+				if (!defaultValue.isString()) return;
+				defaultValue = defaultValue.string;
+				break;
+			case 1:
+				param = this.evaluateExpression(expr.arguments[0]);
+				if (!param.isString()) return;
+				defaultValue = param = param.string;
+				break;
+			default:
+				return;
 		}
-		var result = localization ? localization(param) : defaultValue;
-		if(typeof result == "undefined") {
+
+		var result = localization(param);
+
+		if (typeof result == "undefined") {
 			var error = this.state.module[__dirname];
-			if(!error) {
+			if (!error) {
 				error = this.state.module[__dirname] = new MissingLocalizationError(this.state.module, param, defaultValue);
-				if (failOnMissing) {
-					this.state.module.errors.push(error);
-				} else {
-					this.state.module.warnings.push(error);
-				}
-			} else if(error.requests.indexOf(param) < 0) {
+				this.state.module.warnings.push(error);
+			} else if (error.requests.indexOf(param) < 0) {
 				error.add(param, defaultValue);
 			}
-			result = defaultValue;
+
+			if (fallback) {
+				result = fallback(param);
+
+				if (typeof result == "undefined") {
+					error = this.state.module[__dirname];
+					if (!error) {
+						error = this.state.module[__dirname] = new MissingLocalizationError(this.state.module, param, defaultValue);
+						this.state.module.errors.push(error);
+					} else if (error.requests.indexOf(param) < 0) {
+						error.add(param, defaultValue);
+					}
+					result = defaultValue;
+				}
+
+			} else {
+				result = defaultValue;
+			}
 		}
+
 		var dep = new ConstDependency(JSON.stringify(result), expr.range);
 		dep.loc = expr.loc;
 		this.state.current.addDependency(dep);
@@ -77,20 +92,20 @@ I18nPlugin.prototype.apply = function(compiler) {
  * @returns {*}
  */
 function byString(object, stringKey) {
-    stringKey = stringKey.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    stringKey = stringKey.replace(/^\./, ''); // strip a leading dot
+	stringKey = stringKey.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+	stringKey = stringKey.replace(/^\./, ''); // strip a leading dot
 
-    var keysArray = stringKey.split('.');
-    for (var i = 0, length = keysArray.length; i < length; ++i) {
-      var key = keysArray[i];
-        if (key in object) {
-          object = object[key];
-        } else {
-          return;
-        }
-    }
+	var keysArray = stringKey.split('.');
+	for (var i = 0, length = keysArray.length; i < length; ++i) {
+		var key = keysArray[i];
+		if (key in object) {
+			object = object[key];
+		} else {
+			return;
+		}
+	}
 
-    return object;
+	return object;
 }
 
 /**
@@ -99,7 +114,7 @@ function byString(object, stringKey) {
  * @returns {Function}
  */
 function makeLocalizFunction(localization) {
-  return function localizFunction(key) {
-    return byString(localization, key);
-  };
+	return function localizFunction(key) {
+		return byString(localization, key);
+	};
 }
